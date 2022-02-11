088a9bae7bb9232f1d9c4e581fa4a96d63b8289b
Nghiem Le <ducnghiem2411@gmail.com>
2022-02-11 10:44:36 +0700

    update buff params

--- src/graphql/mutations/fusion.ts
+++ src/graphql/mutations/fusion.ts
@@ -3,14 +3,18 @@ import { getAccountFromPrivateKey } from "../../web3"
 
 export async function fusion(root: any, args: any, ctx: any) {
     try {
-        const { runeType, times, privateKey } = args
+        const { runeType, times, buff, privateKey } = args
 
         const { address } = getAccountFromPrivateKey(privateKey)
+        
+        if (buff !== 1 || buff !== 2) {
+            throw new Error('Buff must be equal to 1 or 2')
+        }
 
         let successTimes = 0
         let failedTimes = 0
         for (let i = 0; i < times; i++) {
-            let res = await fusionRune(address, runeType)
+            let res = await fusionRune(address, buff, runeType)
             console.log(res)
             if (res) {
                 successTimes++
--- src/services/fusion.ts
+++ src/services/fusion.ts
@@ -1,9 +1,10 @@
 import { GAS } from "../config";
 import { fusionContract } from "../web3";
 
-export async function fusionRune(address: string, token: number) {
+export async function fusionRune(address: string, token: number, buff: 1 | 2) {
     try {
-        return await fusionContract.methods.fusion(token, [0, 0]).send({ from: address, gas: GAS })
+        const buffArray = buff === 1 ? [0] : [0, 0]
+        return await fusionContract.methods.fusion(token, buffArray).send({ from: address, gas: GAS })
     } catch (error) {
         
     }
