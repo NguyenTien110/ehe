7b6908e5af6b9f3a5158a503968847702beaa158
Nghiem Le <ducnghiem2411@gmail.com>
2022-02-11 10:53:49 +0700

    show buff

--- src/graphql/mutations/fusion.ts
+++ src/graphql/mutations/fusion.ts
@@ -6,17 +6,20 @@ export async function fusion(root: any, args: any, ctx: any) {
         const { runeType, times, buff, privateKey } = args
 
         const { address } = getAccountFromPrivateKey(privateKey)
-        
-        if (buff !== 1 || buff !== 2) {
-            throw new Error('Buff must be equal to 1 or 2')
+
+        if (buff !== 1 && buff !== 2 && buff !== 0) {
+            throw new Error('Buff must be equal to 0, 1 or 2')
         }
 
         let successTimes = 0
         let failedTimes = 0
+        let buffArray: any
+
         for (let i = 0; i < times; i++) {
             let res = await fusionRune(address, buff, runeType)
             console.log(res)
-            if (res) {
+            if (res?.result) {
+                buffArray = res.buff
                 successTimes++
             } else {
                 failedTimes++
@@ -26,7 +29,8 @@ export async function fusion(root: any, args: any, ctx: any) {
         return {
             runeType,
             successTimes,
-            failedTimes
+            failedTimes,
+            buff: buffArray
         }
     } catch (e) {
         throw e
--- src/services/fusion.ts
+++ src/services/fusion.ts
@@ -1,10 +1,19 @@
 import { GAS } from "../config";
 import { fusionContract } from "../web3";
 
-export async function fusionRune(address: string, token: number, buff: 1 | 2) {
+export async function fusionRune(address: string, token: number, buff: 0 | 1 | 2) {
     try {
-        const buffArray = buff === 1 ? [0] : [0, 0]
-        return await fusionContract.methods.fusion(token, buffArray).send({ from: address, gas: GAS })
+        let buffArray: number[] = []
+
+        for (let i = 0; i < buff; i++) {
+            buffArray.push(0)
+        }
+
+        const res = await fusionContract.methods.fusion(token, buffArray).send({ from: address, gas: GAS })
+        return {
+            buff: buffArray,
+            result: res
+        }
     } catch (error) {
         
     }
